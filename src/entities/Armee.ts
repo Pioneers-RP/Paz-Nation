8008ade3834208347693f5d6fda4e92f50d0b31e
Jerome Morgado <jerome.morgado@me.com>
2023-10-15 20:02:39 +0200

    test

--- src/entities/Armee.ts
+++ src/entities/Armee.ts
@@ -1,52 +1,69 @@
-import {Column, Entity, Index, PrimaryGeneratedColumn} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Index("id_joueur", ["idJoueur"], { unique: true })
-@Entity("armee", { schema: "customer_355631_test" })
+@Entity()
 export class Armee {
-  @PrimaryGeneratedColumn({ type: "int", name: "id_pays" })
-  idPays: number;
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id_pays' })
+  idPays: number | null;
 
-  @Column("varchar", {
-    name: "id_joueur",
-    nullable: true,
+  @Column('varchar', {
+    name: 'id_joueur',
+    nullable: true, // Champ non obligatoire
     unique: true,
     length: 255,
   })
   idJoueur: string | null;
 
-  @Column( { name: "strategie", default: () => "'defense'" })
+  @Column({ name: 'strategie', default: () => "'defense'" })
   strategie: string;
 
-  @Column("int", { name: "avion", default: () => "'0'" })
-  avion: number;
+  @Column('int', { name: 'avion', default: () => '0', nullable: true }) // Champ non obligatoire
+  avion: number | null;
 
-  @Column("int", { name: "equipement_support", default: () => "'0'" })
-  equipementSupport: number;
+  @Column('int', { name: 'equipement_support', default: () => '0', nullable: true }) // Champ non obligatoire
+  equipementSupport: number | null;
 
-  @Column("int", { name: "materiel_infanterie", default: () => "'0'" })
-  materielInfanterie: number;
+  @Column('int', { name: 'materiel_infanterie', default: () => '0', nullable: true }) // Champ non obligatoire
+  materielInfanterie: number | null;
 
-  @Column("int", { name: "vehicule", default: () => "'0'" })
-  vehicule: number;
+  @Column('int', { name: 'vehicule', default: () => '0', nullable: true }) // Champ non obligatoire
+  vehicule: number | null;
 
-  @Column("int", { name: "unite", default: () => "'0'" })
-  unite: number;
+  @Column('int', { name: 'unite', default: () => '0', nullable: true }) // Champ non obligatoire
+  unite: number | null;
 
-  @Column("int", { name: "aviation", default: () => "'0'" })
-  aviation: number;
+  @Column('int', { name: 'aviation', default: () => '0', nullable: true }) // Champ non obligatoire
+  aviation: number | null;
 
-  @Column("int", { name: "infanterie", default: () => "'0'" })
-  infanterie: number;
+  @Column('int', { name: 'infanterie', default: () => '0', nullable: true }) // Champ non obligatoire
+  infanterie: number | null;
 
-  @Column("int", { name: "mecanise", default: () => "'0'" })
-  mecanise: number;
+  @Column('int', { name: 'mecanise', default: () => '0', nullable: true }) // Champ non obligatoire
+  mecanise: number | null;
 
-  @Column("int", { name: "support", default: () => "'0'" })
-  support: number;
+  @Column('int', { name: 'support', default: () => '0', nullable: true }) // Champ non obligatoire
+  support: number | null;
 
-  @Column("int", { name: "victoire", default: () => "'0'" })
-  victoire: number;
+  @Column('int', { name: 'victoire', default: () => '0', nullable: true }) // Champ non obligatoire
+  victoire: number | null;
 
-  @Column("int", { name: "defaite", default: () => "'0'" })
-  defaite: number;
+  @Column('int', { name: 'defaite', default: () => '0', nullable: true }) // Champ non obligatoire
+  defaite: number | null;
+
+  constructor() {
+    this.idPays = null;
+    this.idJoueur = null;
+    this.strategie = "";
+    this.avion = null;
+    this.equipementSupport = null;
+    this.materielInfanterie = null;
+    this.vehicule = null;
+    this.unite = null;
+    this.aviation = null;
+    this.infanterie = null;
+    this.mecanise = null;
+    this.support = null;
+    this.victoire = null;
+    this.defaite = null;
+  }
+
 }
---
+++ src/test.ts
@@ -0,0 +1,48 @@
+import { DataSource } from 'typeorm';
+import { Armee } from './entities/Armee'; // Assurez-vous que le chemin est correct
+
+const dataSource = new DataSource({
+  type: 'mariadb',
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3306,
+  username: process.env.USER || 'root',
+  password: process.env.PASSWORD || 'password',
+  database: process.env.DATABASE || 'database',
+  entities: [
+    __dirname + "/entity/*.ts"
+  ],
+  synchronize: true,
+  multipleStatements: true,
+});
+
+async function main() {
+  try {
+    const connection = await dataSource.connect();
+
+    console.log('1');
+    // Crée une nouvelle Armee et l'ajoute à la base de données
+    const armeeRepository = connection.getRepository(Armee);
+    console.log('2');
+    const nouvelleArmee = new Armee();      
+    console.log('3');
+    nouvelleArmee.idJoueur = "175618172743974912";
+    console.log('4');
+    nouvelleArmee.avion = 1000;
+    console.log('5');
+    await armeeRepository.save(nouvelleArmee);
+    console.log('Armee enregistrée avec succès !');
+
+    // Recherche et affiche le contenu de la colonne 'avion'
+    // const armeeEnregistree = await armeeRepository.findOne({ idJoueur: nouvelleArmee.idPays });
+    // if (armeeEnregistree) {
+    // console.log('Contenu de la colonne "avion":', armeeEnregistree.avion);
+    // } else {
+    // console.log('Armee non trouvée.');
+    // }
+
+    // Ferme la connexion à la base de données
+    await connection.close();
+  } catch (error) {
+    console.error('Une erreur s\'est produite :', error);
+  }
+}
