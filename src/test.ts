2b2095661c601be0337fc0d327d7d4467540c4c5
Jerome Morgado <jerome.morgado@me.com>
2023-10-15 20:09:26 +0200

    dotenv + log

--- src/test.ts
+++ src/test.ts
@@ -1,13 +1,14 @@
 import { DataSource } from 'typeorm';
 import { Armee } from './entities/Armee'; // Assurez-vous que le chemin est correct
-
+import dotenv from 'dotenv';
+dotenv.config();
 const dataSource = new DataSource({
-  type: 'mariadb',
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3306,
-  username: process.env.USER || 'root',
-  password: process.env.PASSWORD || 'password',
-  database: process.env.DATABASE || 'database',
+    type: 'mariadb',
+    host: process.env.HOST || 'localhost',
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3306,
+    username: process.env.USER || 'root',
+    password: process.env.PASSWORD || 'password',
+    database: process.env.DATABASE || 'database',
   entities: [
     __dirname + "/entity/*.ts"
   ],
@@ -17,20 +18,21 @@ const dataSource = new DataSource({
 
 async function main() {
   try {
+    console.log(JSON.stringify(process.env));
     const connection = await dataSource.connect();
-
+    console.log(JSON.stringify(connection));
     console.log('1');
     // Crée une nouvelle Armee et l'ajoute à la base de données
     const armeeRepository = connection.getRepository(Armee);
     console.log('2');
-    const nouvelleArmee = new Armee();      
-    console.log('3');
-    nouvelleArmee.idJoueur = "175618172743974912";
-    console.log('4');
-    nouvelleArmee.avion = 1000;
-    console.log('5');
-    await armeeRepository.save(nouvelleArmee);
-    console.log('Armee enregistrée avec succès !');
+    // const nouvelleArmee = new Armee();      
+    // console.log('3');
+    // nouvelleArmee.idJoueur = "175618172743974912";
+    // console.log('4');
+    // nouvelleArmee.avion = 1000;
+    // console.log('5');
+    // await armeeRepository.save(nouvelleArmee);
+    // console.log('Armee enregistrée avec succès !');
 
     // Recherche et affiche le contenu de la colonne 'avion'
     // const armeeEnregistree = await armeeRepository.findOne({ idJoueur: nouvelleArmee.idPays });
@@ -46,3 +48,5 @@ async function main() {
     console.error('Une erreur s\'est produite :', error);
   }
 }
+
+main();
