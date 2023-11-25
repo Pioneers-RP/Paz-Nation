a0794d2f44e68435a07d3a8fab2a453557b4c4c6
Jerome Morgado <jerome.morgado@me.com>
2023-11-26 00:55:35 +0100

    orm

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'mariadb',
-  host: process.env.DATABASE_HOST ?? 'localhost',
+  host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : 3306,
-  username: process.env.DATABASE_USER ?? 'root',
-  password: process.env.DATABASE_PASSWORD ?? 'password',
-  database: process.env.DATABASE_DATABASE ?? 'database',
+  username: process.env.DATABASE_USER + '-orm',
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
   synchronize: process.env.NODE_ENV === 'development',
   logging: false,
   entities: [
