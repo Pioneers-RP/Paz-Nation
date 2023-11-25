61482b0dcf7d1b61ad1e31c3117975f1baa7650a
Jerome Morgado <jerome.morgado@me.com>
2023-11-26 00:38:00 +0100

    fix: logs

--- src/OLD/index.ts
+++ src/OLD/index.ts
@@ -6,10 +6,10 @@ import mysql from 'mysql';
 
 console.log("Starting1 Index.ts")
 console.log("process.env.DATABASE_HOST : " + process.env.DATABASE_HOST)
+console.log("process.env.DATABASE_PORT : " + process.env.DATABASE_PORT)
 console.log("process.env.DATABASE_USER : " + process.env.DATABASE_USER)
 console.log("process.env.DATABASE_PASSWORD : " + process.env.DATABASE_PASSWORD)
 console.log("process.env.DATABASE_DATABASE : " + process.env.DATABASE_DATABASE)
-console.log("process.env.DATABASE_PORT : " + process.env.DATABASE_PORT)
 console.log("process.env.TOKEN : " + process.env.TOKEN)
 console.log("Ending1 Index.ts")
 
