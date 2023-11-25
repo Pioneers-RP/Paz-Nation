ae1e281f833fbef08356fffbd812628c2eabfb03
Jerome Morgado <jerome.morgado@me.com>
2023-11-25 21:05:17 +0100

    chore: add port + defaults

--- src/OLD/data-source.ts
+++ src/OLD/data-source.ts
@@ -5,6 +5,7 @@ console.log("process.env.DATABASE_HOST : " + process.env.DATABASE_HOST)
 console.log("process.env.DATABASE_USER : " + process.env.DATABASE_USER)
 console.log("process.env.DATABASE_PASSWORD : " + process.env.DATABASE_PASSWORD)
 console.log("process.env.DATABASE_DATABASE : " + process.env.DATABASE_DATABASE)
+console.log("process.env.DATABASE_PORT : " + process.env.DATABASE_PORT)
 console.log("process.env.TOKEN : " + process.env.TOKEN)
 console.log("Ending1 Datasource1.ts")
 
--- src/OLD/index.ts
+++ src/OLD/index.ts
@@ -9,6 +9,7 @@ console.log("process.env.DATABASE_HOST : " + process.env.DATABASE_HOST)
 console.log("process.env.DATABASE_USER : " + process.env.DATABASE_USER)
 console.log("process.env.DATABASE_PASSWORD : " + process.env.DATABASE_PASSWORD)
 console.log("process.env.DATABASE_DATABASE : " + process.env.DATABASE_DATABASE)
+console.log("process.env.DATABASE_PORT : " + process.env.DATABASE_PORT)
 console.log("process.env.TOKEN : " + process.env.TOKEN)
 console.log("Ending1 Index.ts")
 
@@ -16,12 +17,13 @@ export const connect = AppDataSource.initialize()
     .then(() => {
         console.log("Database initialized with TypeORM")})
     .catch((error) => console.log(error))
+
 export const connection = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DATABASE,
-    port: Number('3306'),
+    host: process.env.DATABASE_HOST ?? 'localhost',
+    port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT, 10) : 3306,
+    user: process.env.DATABASE_USER ?? 'root',
+    password: process.env.DATABASE_PASSWORD ?? 'password',
+    database: process.env.DATABASE_DATABASE ?? 'database',
     multipleStatements: true
 })
 
